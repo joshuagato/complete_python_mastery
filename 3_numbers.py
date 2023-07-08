c154c535dc4943e603ee1b3a67d755782a85d93d
Joshua Gato <joshuagato37@gmail.com>
2023-07-08 08:32:52 +0000

    Added numbers

---
+++ 3_numbers.py
@@ -0,0 +1,14 @@
+import math
+
+print(10 + 3)   # Addition
+print(10 - 3)   # Subtraction
+print(10 * 3)   # Multiplication
+print(10 / 3)   # Division
+print(10 // 3)  # Integer division
+print(10 % 3)   # Modulo/Remainder division
+print(10 ** 3)  # Exponentiation
+
+print(round(2.9))
+print(abs(-2.9))
+
+print(math.ceil(2.2))
