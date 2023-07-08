1047e73867d9a089497c55d820d1d0c56192d5de
Joshua Gato <joshuagato37@gmail.com>
2023-07-08 13:29:03 +0000

    Added chaining comparison operators

--- 2_Flow_Control/8_logical_operator.py
+++ 2_Flow_Control/8_logical_operator.py
@@ -6,3 +6,10 @@ if (high_income or good_credit) and not student:
     print("Eligible")
 else:
     print("Not eligible")
+
+
+# Chaining comparison operators
+age = 22
+# if age >= 18 and age < 65:
+if 18 <= age < 65:
+    print("Eligible")
