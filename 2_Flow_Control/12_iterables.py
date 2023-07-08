e1bad52d52b49dbf65d2a32d23135f91573c14d1
Joshua Gato <joshuagato37@gmail.com>
2023-07-08 15:43:55 +0000

    Added iterables

---
+++ 2_Flow_Control/12_iterables.py
@@ -0,0 +1,13 @@
+print(type(5))  # <class 'int'>
+print(type(range(5)))  # <class 'range'>
+
+# Iterable
+for x in "Python":
+    print(x, end=" ", sep="")
+
+print()
+
+for x in [1, 2, 3, 4]:
+    print(x, end=" ", sep="")
+
+print()
