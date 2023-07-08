4bc991ac1aa675847a9d4a37766e646de8f799ec
Joshua Gato <joshuagato37@gmail.com>
2023-07-08 08:54:14 +0000

    Added type conversion

---
+++ 4_type_conversion.py
@@ -0,0 +1,23 @@
+x = input("X: ")
+
+y = int(x) + 1
+
+print(f"x: {x}, y: {y}")
+
+print(type(x))
+
+int(x)
+float(x)
+bool(x)
+str(x)
+
+# Falsy
+print(bool(0))
+print(bool(""))
+print(bool(None))
+
+# Truthy
+print(bool(1))
+print(bool(-1))
+print(bool(5))
+print(bool("False"))
