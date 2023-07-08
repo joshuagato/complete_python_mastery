bf22c41151d942a4d1081ef1869c64b46be385f8
Joshua Gato <joshuagato37@gmail.com>
2023-07-08 10:53:54 +0000

    Restructured source code

---
+++ 2_Flow_Control/5_comparison_operators.py
@@ -0,0 +1,20 @@
+print(10 > 3)  # True
+
+print(10 >= 3)  # True
+
+print(10 < 20)  # True
+
+print(10 <= 20)  # True
+
+print(10 == 10)  # True
+
+print(10 == "10")  # False
+
+print(10 != "10")  # True
+
+print("bag" > "apple")  # True
+
+print("bag" == "BAG")  # False
+
+print(ord('b'))  # 98 (Numeric representation)
+print(ord('B'))  # 66 (Numeric representation)
