3c0b23ddeebafaf24dd571ad1e633ede23210538
Joshua Gato <joshuagato37@gmail.com>
2023-07-08 11:10:53 +0000

    Added the ternary expression

---
+++ 2_Flow_Control/7_ternary_operator.py
@@ -0,0 +1,12 @@
+age = 22
+
+if age >= 18:
+    message = "Eligible"
+else:
+    message = "Not eligible"
+
+print(message)
+
+# The Ternary Expression
+message = "Eligible" if age >= 18 else "Not eligible"
+print(message)
