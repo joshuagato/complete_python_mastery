f147bcbe27b97eefb01e4177547f3a72c80a09b5
Joshua Gato <joshuagato37@gmail.com>
2023-07-09 15:00:56 +0000

    Added lists

---
+++ 4_Data_Structures/01_LIsts.py
@@ -0,0 +1,14 @@
+letters = ["a", "b", "c", "d"]
+matrix = [[0, 1], [2, 3]]
+zeros = [0] * 5
+combined = zeros + letters
+chars = list("Hello World")
+chars_length = len(chars)
+
+numbers = list(range(1, 21))
+
+print(numbers)
+print(zeros)
+print(combined)
+print(chars)
+print(chars_length)
