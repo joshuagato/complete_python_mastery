86c59ee2f1c29a5405b685e2d6ac3436c1cca714
Joshua Gato <joshuagato37@gmail.com>
2023-07-10 01:42:30 +0000

    Edited the lists module

--- 4_Data_Structures/01_lists.py
+++ 4_Data_Structures/01_lists.py
@@ -1,10 +1,19 @@
 letters = ["a", "b", "c", "d"]
+
+# Two-Dimensional list
 matrix = [[0, 1], [2, 3]]
+
+# List Multiplication
 zeros = [0] * 5
+
+# List Concatenation
 combined = zeros + letters
+
+# Converting String to List
 chars = list("Hello World")
 chars_length = len(chars)
 
+# Creating list from range function
 numbers = list(range(1, 21))
 
 print(numbers)
