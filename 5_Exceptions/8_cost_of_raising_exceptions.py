99d3eb8c01ebb5509932bdc772032a9c5f84416a
Joshua Gato <joshuagato37@gmail.com>
2023-07-12 03:36:45 +0000

    modified 8_cost_of_raising_exceptions

--- 5_Exceptions/8_cost_of_raising_exceptions.py
+++ 5_Exceptions/8_cost_of_raising_exceptions.py
@@ -1,4 +1,9 @@
 # When writing your own functions, prefer not to raise exceptions because they come at a cost
+# When building an application where performance and scalability are important, then it is better to raise exceptions
+#       when you really have to
+# A a general rule of thumb, when you want to raise exceptions in your functions, think twice. See if you can handle
+#       the situation with a simple if statement. Whether there is a performance difference or not, your code will
+#       end up a little bit cleaner. So, raise exceptions if your really have to.
 
 from timeit import timeit
 
